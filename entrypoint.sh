60c8f12736abf11ebc4dbf0eb7a29a58369700f7
Maxence Maireaux <maxence@maireaux.fr>
2021-12-02 23:02:29 +0100

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,20 +14,18 @@ echo "INPUT_RULESET_FILE set to $INPUT_RULESET_FILE"
 POSSIBLE_TEMPLATES=$(grep --with-filename --recursive 'Resources' ${INPUT_CFN_DIRECTORY}/* | cut -d':' -f1 | sort -u)
 
 for f in $POSSIBLE_TEMPLATES; do
-    echo "Checking for ruleset matching template file: ${f}"
-    rules=${f%.*}.ruleset
-    if [ -e $rules ]; then
-        echo "                                      Found: $rules"
-        cg_cmd="cfn-guard validate --rules $rules  --template ${PWD}/${f}"
-        echo "Running command:"
-        echo "$ $cg_cmd"
-        $cg_cmd
-        if [ $? -ne 0 ]; then
-            echo "CFN GUARD FAIL!"
-            exit 1
-        fi
-    else
-        echo "No matching: $rules"
+    if [[ ${f} == *".yml"* ]]; then
+      echo "Checking for ruleset matching template file: ${f}"
+      rules=${f%.*}.ruleset
+      echo "                                      Found: $rules"
+      cg_cmd="cfn-guard validate -d ${f} -r $rules"
+      echo "Running command:"
+      echo "$ $cg_cmd"
+      $cg_cmd
+      if [ $? -ne 0 ]; then
+          echo "CFN GUARD FAIL!"
+          exit 1
+      fi
     fi
 done
 
